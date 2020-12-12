04fd831512ad9d153be55f2d62177f5a50ea1ea6
Jordi <byflowee@gmail.com>
2020-12-12 01:17:27 +0100

    day 11 generic neighbourhood matrix parsing

--- day_11/day_11_2.cpp
+++ day_11/day_11_2.cpp
@@ -18,14 +18,13 @@ int main(int argc, char *argv[])
 
     char *it = input;
     vector<vector<char>> seats;
-    vector<pair<int,int>> neighbours {{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}};
     unsigned i {}, n {};
 
     while (*it != '\n') ++it;
     n = it - input;
 
     seats.push_back(vector<char>());
-    for (auto i = 0; i < n+2; ++i) seats.back().push_back('.');
+    for (auto i = 0; i < n+2; ++i) seats.back().push_back('0');
 
     it = input;
 
@@ -34,22 +33,36 @@ int main(int argc, char *argv[])
         unsigned j {};
         seats.push_back(vector<char>());
 
-        seats.back().push_back('.');
+        seats.back().push_back('0');
         while (*it != '\n') seats.back().push_back(*(it++));
-        seats.back().push_back('.');
+        seats.back().push_back('0');
 
         ++it;
     }
 
     seats.push_back(vector<char>());
-    for (auto i = 0; i < n+2; ++i) seats.back().push_back('.');
+    for (auto i = 0; i < n+2; ++i) seats.back().push_back('0');
 
     vector<vector<char>> dbuffer(seats);
 
     vector<vector<char>>* s1 = &seats;
     vector<vector<char>>* s2 = &dbuffer;
     vector<vector<char>>* aux = &dbuffer;
+    vector<pair<int,int>> neighbours {{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}};
     unsigned changes {};
+
+    auto find_seat = [](int ix, int iy, pair<int,int> dir, vector<vector<char>>* s)
+    {
+        do
+        {
+            iy += dir.second;
+            ix += dir.first;
+
+            if (s->at(ix)[iy] == '0') return false;
+            if (s->at(ix)[iy] == 'L') return false;
+            if (s->at(ix)[iy] == '#') return true;
+        } while (true);
+    };
 
     do
     {
@@ -59,12 +72,11 @@ int main(int argc, char *argv[])
             for (auto y = 1; y <= n ; ++y)
             {
                 int adjacents {};
-                for (auto neighbour : neighbours){
-                    if (s1->at(x+neighbour.first)[y+neighbour.second] == '#') ++adjacents;
-                }
+                for (auto neighbour : neighbours)
+                    if (find_seat(x, y, neighbour, s1)) ++adjacents;
 
                 if (s1->at(x)[y] == 'L' && adjacents == 0) { s2->at(x)[y] = '#'; ++changes; }
-                else if (s1->at(x)[y] == '#' && adjacents >= 4) { s2->at(x)[y] = 'L'; ++changes; }
+                if (s1->at(x)[y] == '#' && adjacents >= 5) { s2->at(x)[y] = 'L'; ++changes; }
             }
         }
 
